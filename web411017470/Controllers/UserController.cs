db3ebda88dfbaa00237768eea47863854bf6931c
jacky60677 <jacky60677@gmail.com>
2023-05-17 20:49:05 +0800

    20230517-2

---
+++ web411017470/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web411017470.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Signup()
+        {
+            return Signup();
+        }
+
+        
+    }
+}
