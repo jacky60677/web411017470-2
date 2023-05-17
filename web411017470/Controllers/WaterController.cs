63e3844f6e69943246cc2810093c813405485a9d
jacky60677 <jacky60677@gmail.com>
2023-05-17 19:56:42 +0800

    20230517

---
+++ web411017470/Controllers/WaterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web411017470.Controllers
+{
+    public class WaterController : Controller
+    {
+        // GET: Water
+        public ActionResult Index(string name,float? weight)
+        {
+            var result = " ";
+            if(weight.HasValue)
+            {
+                result = (weight*30)+"CC";
+            }
+            ViewBag.name = name;
+            ViewBag.result = result;
+            return View();
+        }
+    }
+}
---
+++ web411017470/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web411017470.MvcApplication" Language="C#" %>
